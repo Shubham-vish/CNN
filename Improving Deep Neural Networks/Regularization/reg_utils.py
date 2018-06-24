d0084388cbfd80a4d0a19ff16e5ffde528483443
Shubham Vishwakarma <shubhamvishwakarma2001@gmail.com>
2018-06-24 11:09:29 +0530

    course2 week1 exercise2

--- Improving Deep Neural Networks/Regularization/reg_utils.py
+++ Improving Deep Neural Networks/Regularization/reg_utils.py
@@ -323,14 +323,4 @@ def plot_decision_boundary(model, X, y):
     plt.xlabel('x1')
     plt.scatter(X[0, :], X[1, :], c=y, cmap=plt.cm.Spectral)
     plt.show()
-    
-def load_2D_dataset():
-    data = scipy.io.loadmat('datasets/data.mat')
-    train_X = data['X'].T
-    train_Y = data['y'].T
-    test_X = data['Xval'].T
-    test_Y = data['yval'].T
-
-    plt.scatter(train_X[0, :], train_X[1, :], c=train_Y, s=40, cmap=plt.cm.Spectral);
-    
-    return train_X, train_Y, test_X, test_Y
+   
